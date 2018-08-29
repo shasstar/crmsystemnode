c6c297a6af5dd8d3e6d211e0b2a94781ab131ee1
Shashank Shrivastava <shasstar@gmail.com>
2018-08-29 15:09:46 +0530

    CORS, Authentication, Authorization Added.

--- hosting/crm-system-api-hosting.js
+++ hosting/crm-system-api-hosting.js
@@ -1,22 +1,33 @@
 const ErrorConstants = require('../constants').ErrorConstants;
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const AppRouters = require('../routing');
 const express = require('express');
 const expressJwt = require('express-jwt');
 
 class CRMSystemAPIHost {
-    constructor(portNumber, globalSecretKey) {
+    constructor(portNumber, globalSecretKey, isHttpsEnabled, httpsSecurityOptions, isAuthEnabled) {
         const validation = portNumber && globalSecretKey;
 
         if (!validation) {
             throw new Error(ErrorConstants.INVALID_ARGUMENTS);
         }
 
-        this.app = express();
-        this.httpServer = http.createServer(this.app);
+        [this.portNumber, this.globalSecretKey, this.isHttpsEnabled, this.httpsSecurityOptions, this.isAuthEnabled] = arguments;
 
-        this.portNumber = portNumber;
-        this.globalSecretKey = globalSecretKey;
+        this.app = new express();
+
+        if (!isHttpsEnabled) {
+            this.webServer = http.createServer(this.app);
+        } else {
+            const serverOptions = {
+                cert: fs.readFileSync(this.httpsSecurityOptions.certificateFile),
+                key: fs.readFileSync(this.httpsSecurityOptions.keyFile),
+                passphrase: this.httpsSecurityOptions.sslPassPhrase
+            };
+            this.webServer = https.createServer(serverOptions, this.app);
+        }
 
         this.customerRouter = new AppRouters.CustomerRouter();
         this.orderRouter = new AppRouters.OrderRouter();
@@ -25,12 +36,24 @@ class CRMSystemAPIHost {
         this.initializeHost();
     }
 
+    addCORSHeaders(request, response, next) {
+        if (response) {
+            response.header('Access-Control-Allow-Credentials', 'true');
+            response.header('Access-Control-Allow-Origin', '*');
+            response.header('Access-Control-Allow-Methods', '*');
+            response.header('Access-Control-Allow-Headers', 'Origin X-Requested-With Content-Type Accept Authorization');
+        }
+        next();
+    }
+
     initializeHost() {
+        this.app.use(this.addCORSHeaders);
         this.app.use(this.verifyAuthorization);
 
-        this.app.use('/api/customers', this.getExpressJwtSnippet());
-
-        this.app.use('/api/orders', this.getExpressJwtSnippet());
+        if (this.isAuthEnabled) {
+            this.app.use('/api/customers', this.getExpressJwtSecretKey());
+            this.app.use('/api/orders', this.getExpressJwtSecretKey());
+        }
 
         this.app.use(express.json());
         this.app.use(express.urlencoded({
@@ -39,7 +62,7 @@ class CRMSystemAPIHost {
 
         this.app.use('/api/customers', this.customerRouter.router);
         this.app.use('/api/orders', this.orderRouter.router);
-        this.app.use('/api/authenticate', this.userProfileRouter.router);
+        this.app.use('/authenticate', this.userProfileRouter.router);
     }
 
     verifyAuthorization(error, request, response, next) {
@@ -51,18 +74,18 @@ class CRMSystemAPIHost {
         next();
     }
 
-    getExpressJwtSnippet() {
+    getExpressJwtSecretKey() {
         return expressJwt({
             secret: this.globalSecretKey
         });
     }
 
     async start() {
-        this.httpServer.listen(this.portNumber);
+        this.webServer.listen(this.portNumber);
     }
 
     async stop() {
-        this.httpServer.close();
+        this.webServer.close();
     }
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const Hosts = require('./hosting');
+const argv = require('minimist')(process.argv.splice(2), {
+    boolean: true
+});
 
-const DEFAULT_HOST = 'localhost';
 const DEFAULT_PORT = 9000;
+const DEFAULT_SECRET_KEY = 'PwC,Bangalore';
+const DEFAULT_HTTPS_OPTION = false;
+const DEFAULT_CERT_FILE = 'cert.pem';
+const DEFAULT_KEY_FILE = 'key.pem';
+const DEFAULT_AUTH_OPTION = false;
+const DEFAULT_LOG_OPTION = 'tiny';
 
-const hostName = process.env.HOST || DEFAULT_HOST;
-const portNumber = process.env.PORT || DEFAULT_PORT;
+const portNumber = argv['port-number'] || DEFAULT_PORT;
+const globalSecretKey = argv['global-secret'] || DEFAULT_SECRET_KEY;
+const isHttpsEnabled = argv['enable-https'] || DEFAULT_HTTPS_OPTION;
+const keyFile = argv['key-file'] || DEFAULT_KEY_FILE;
+const certificateFile = argv['cert-file'] || DEFAULT_CERT_FILE;
+const sslPassPhrase = argv['ssl-passphrase'];
+const isAuthenticationEnabled = argv['enable-jwt-auth'] || DEFAULT_AUTH_OPTION;
+const logOption = argv['log-option'] || DEFAULT_LOG_OPTION;
 
-const CRMSystemApiHost = new Hosts.CRMSystemAPIHost(portNumber, hostName);
+const httpsSecurityOptions = {
+    keyFile,
+    certificateFile,
+    sslPassPhrase
+};
+
+const hostOptions = {
+    portNumber,
+    globalSecretKey,
+    isHttpsEnabled,
+    httpsSecurityOptions,
+    isAuthenticationEnabled,
+    logOption
+}
+
+// Writing more numbers of parameters is not a good design, thats why hostOptions object is created.
+
+// const CRMSystemApiHost = new Hosts.CRMSystemAPIHost(portNumber, globalSecretKey, isHttpsEnabled,
+//     httpsSecurityOptions, isAuthenticationEnabled, logOption);
+
+const CRMSystemApiHost = new Hosts.CRMSystemAPIHost(hostOptions);
 
 CRMSystemApiHost.start().then(
     () => console.log('Server started successfully'),
@@ -15,7 +49,7 @@ CRMSystemApiHost.start().then(
 
 const exitHandler = () => {
     () => console.log('Server stoppped successfully'),
-    () => console.log('Unable to stop the server')
+        () => console.log('Unable to stop the server')
 };
 
 process.on('exit', exitHandler);
